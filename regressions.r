62a58f2eb25e9cd6149806e7e4a5efcfb785fc48
sonanmemon <sonanahmed8@gmail.com>
2024-08-14 21:46:51 +0500

    c

--- regressions.r
+++ regressions.r
@@ -20,6 +20,8 @@ library(broom)
 
 library(modelsummary)
 
+library(stargazer)
+
 
 
 theme_set(hrbrthemes::theme_ipsum())
@@ -58,26 +60,26 @@ ols1_robust
 
 
 
+
+# Newey West VCOV:
+
+ols1_hac = lmtest::coeftest(ols1, vcov = NeweyWest)
+ols1_hac
+
+# NeweyWest Standard Errors:
+
 NeweyWest(ols1) ## Print the HAC VCOV
 
 sqrt(diag(NeweyWest(ols1))) ## Print the HAC SEs
 
 
 
-# Regression Table Output:
-
-msummary(list(ols1, ols1_robust))
-
-
-
-ols1_hac = lmtest::coeftest(ols1, vcov = NeweyWest)
-ols1_hac
-
 
 # clustered standard errors:
 
 
 lm_robust(mass ~ height, data = starwars, clusters = homeworld)
+
 
 
 # Interaction Effect:
@@ -93,42 +95,87 @@ ols_fe = feols(mass ~ height | species, data = starwars)
 ols_fe
 
 
-# Specifying Standard Errors:
 
-#feols(mass ~ height | species,
-  #    data = starwars, se = 'standard')
 
 
 ## We now have two fixed effects: species and homeworld
-ols_hdfe = feols(mass ~ height | species + homeworld, data = starwars)
-ols_hdfe
+
+ols_2fe = feols(mass ~ height | species + homeworld, data = starwars)
+ols_2fe
 
 
 
 
-ols_hdfe = summary(ols_hdfe, cluster = ~ species + homeworld) ## I'll go with this one
-ols_hdfe
+ols_2fe = summary(ols_2fe, cluster = ~ species + homeworld) ## I'll go with this one
+ols_2fe
 
 # Etable:
 
-etable(ols_fe, ols_hdfe)
+etable(ols_fe, ols_2fe)
 
 
 # plotting estimation coefficients:
 
 
-coefplot(list(ols_fe, ols_hdfe))
+coefplot(list(ols_fe, ols_2fe))
 
 ## Add legend (optional)
 legend("bottomleft", col = 1:2, lwd = 1, pch = c(20, 17),
-       legend = c("FE and no clustering", "HDFE and twoway clustering"))
+       legend = c("FE and no clustering", "2 FE and twoway clustering"))
 
 
 
+# Regression Tables:
 
 
-msummary(list(ols1, ols_fe, ols_hdfe), output = "latex")
 
+msummary(list(ols1, ols_fe, ols_2fe), output = "latex")
+
+## or
+
+
+
+stargazer(ols1, ols_ie, 
+          header=FALSE,
+          type= "latex",
+          out = "model_output1.tex",
+          title="Model With and Without Interaction Term",
+          keep.stat="n",digits=2,
+          column.labels = c("Model 1", "Model 2"),
+          intercept.bottom=FALSE)
+
+
+stargazer(ols1, ols_ie,
+          header=FALSE,
+          title="Baseline Vs Baseline With Interaction Term",
+          type= "latex",
+          out = "model_output2.tex",
+          keep.stat="n", # what statistics to print
+          omit.table.layout="n",
+          star.cutoffs=NA,
+          digits=3,
+          intercept.bottom=FALSE, #moves the intercept coef to top
+          column.labels = c("Model 1", "Model 2"),
+          dep.var.labels.include = FALSE,
+          model.numbers = FALSE,
+          dep.var.caption="Mass of Animal",
+          model.names=FALSE,
+          star.char=NULL) #supresses the stars
+
+
+## texreg: better suited for handling multiple types of regression: for e.g. with
+## or without fixed effects.
+
+library(texreg)
+
+
+texreg(list(ols1, ols_fe, ols_2fe),
+       caption = "Regression Results OLS, OLS-FE and OLS-HDFE",
+       label = "tab:models")
+          
+
+# Data Summary Table:
+          
 datasummary_balance(~ gender,
                     data = starwars %>% select(height:mass, birth_year, eye_color, gender))
 
@@ -137,21 +184,11 @@ datasummary_balance(~ gender,
 
 
 
-#https://modelsummary.com/articles/modelsummary.html
 
+# Summary Stats Table:
 
 otype = ifelse(knitr::is_latex_output(), 'return', 'kable')
 
-## st() is an alias for sumtable()
-
-
-
-humans =
-  starwars %>%
-  filter(species=="Human") %>%
-  select(where(Negate(is.list))) ## Drop list columns (optional)
-
-humans
 
 
 
@@ -162,6 +199,7 @@ st(humans %>% select(height:mass, birth_year, eye_color, gender),
 
 
 
+# Correlation Plot:
 
 ggplot(humans, aes(x = height, y = mass)) +
   geom_point(alpha = 0.7) +
@@ -169,7 +207,12 @@ ggplot(humans, aes(x = height, y = mass)) +
 
 
 
+# References:
 
+#https://modelsummary.com/articles/modelsummary.html
+
+
+#https://socviz.co/modeling.html
 
 
 
